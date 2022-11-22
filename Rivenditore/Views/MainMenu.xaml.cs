fdd7224ac2c61409c28f1b1de1dcb9ebd3b5e8c5
Nicolo <govinicolo03@gmail.com>
2022-11-22 11:16:33 +0100

    modifica MainMenu.xaml

--- Rivenditore/Views/MainMenu.xaml.cs
+++ Rivenditore/Views/MainMenu.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Rivenditore.ViewModels;
 
 namespace Rivenditore.Views
 {
@@ -20,9 +21,13 @@ namespace Rivenditore.Views
     /// </summary>
     public partial class MainMenu : Page
     {
+        private MainMenuViewModel vm;
         public MainMenu()
         {
             InitializeComponent();
+            vm = new MainMenuViewModel();
+            this.DataContext = vm;
+            
         }
     }
 }
