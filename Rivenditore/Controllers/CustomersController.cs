6280ddce7030b6b0faec93430b8298fdebbd1395
bibadada <davideregnani@gmail.com>
2022-11-23 12:55:27 +0100

    Controller + ViewModel Customer

---
+++ Rivenditore/Controllers/CustomersController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using Rivenditore.Models;
+
+namespace Rivenditore.Controllers
+{
+    public static class CustomersController
+    {
+        public async static Task<List<Customer>> GetAll()
+        {
+            using(RivenditoreEntities context = new RivenditoreEntities())
+            {
+                try
+                {
+                    List<Customer> test = await context.Customers.ToListAsync();
+                    return test;
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+
+        }
+    }
+}
--- Rivenditore/ViewModels/CustomersViewModel.cs
+++ Rivenditore/ViewModels/CustomersViewModel.cs
@@ -3,11 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Rivenditore.Models;
+using Rivenditore.Controllers;
 
 namespace Rivenditore.ViewModels
 {
     class CustomersViewModel : BaseViewModel
     {
+        #region BindingSource
 
+        private List<Customer> _listaCustomers;
+
+        public List<Customer> ListaCustomers
+        {
+            get { return _listaCustomers; }
+            set{ _listaCustomers = value;
+                NotifyPropretyChanged("ListaCustomers");
+            }
+        }
+
+
+        #endregion
+
+        public CustomersViewModel()
+        {
+            Setup();
+        }
+
+        private async Task Setup()
+        {
+            ListaCustomers = await CustomersController.GetAll();
+        }
     }
 }
