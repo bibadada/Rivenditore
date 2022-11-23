6b98d26ca7ba0d213f8f471c1227841cd3b2d842
Nicolo <govinicolo03@gmail.com>
2022-11-23 14:17:34 +0100

    Setup ViewModel e Controller(GetAll()) degli Orders

---
+++ Rivenditore/Controllers/OrdersController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rivenditore.Models;
+using System.Data.Entity;
+
+namespace Rivenditore.Controllers
+{
+    class OrdersController
+    {
+
+        public async static Task<List<Order>> GetAll()
+        {
+            using (RivenditoreEntities context = new RivenditoreEntities())
+            {
+                try
+                {
+                    return await context.Orders.ToListAsync();
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+        }
+    }
+}
--- Rivenditore/ViewModels/OrdersViewModel.cs
+++ Rivenditore/ViewModels/OrdersViewModel.cs
@@ -3,11 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Rivenditore.Models;
+using Rivenditore.Controllers;
 
 namespace Rivenditore.ViewModels
 {
     class OrdersViewModel : BaseViewModel
     {
+        private List<Order> _listaOrders;
+
+        public List<Order> ListaOrders
+        {
+            get { return _listaOrders; }
+            set { _listaOrders = value;
+                NotifyPropretyChanged("ListaOrders");
+            }
+        }
+
+        public OrdersViewModel()
+        {
+          
+        }
+
+        public async Task Setup()
+        {
+            this.ListaOrders = await OrdersController.GetAll();
+        }
 
     }
 }
