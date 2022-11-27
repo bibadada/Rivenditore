80834fe5ec47687d8d422070569b77897e03f34b
Nicolo <govinicolo03@gmail.com>
2022-11-27 19:28:15 +0100

    implementazione interfaccia IDataError(con dovute modifiche) nel BaseViewModel, creazione e implementazione classe CustomersExtension, proprietà ValidateOnDataError impostata a true nel .xaml dell Customer

---
+++ Rivenditore/Models/CustomerExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rivenditore.Models
+{
+
+    //la calsse Customer avrà i suoi attributi in questa classe
+    [MetadataTypeAttribute(typeof(CustomerExtension))]
+    public partial class Customer
+    {
+
+    }
+
+    class CustomerExtension
+    {
+        public int Id { get; set; }
+        [StringLength(5, ErrorMessage = "il cognomw deve essere minore di 5")]
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public string FiscalCode { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Cap { get; set; }
+        public string Mail { get; set; }
+        public string Phone { get; set; }
+    }
+}
--- Rivenditore/ViewModels/BaseViewModel.cs
+++ Rivenditore/ViewModels/BaseViewModel.cs
@@ -1,19 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Rivenditore.ViewModels
 {
-    public class BaseViewModel : INotifyPropertyChanged
+    public class BaseViewModel : INotifyPropertyChanged, IDataErrorInfo
     {
         public event PropertyChangedEventHandler PropertyChanged;
-
         protected void NotifyPropretyChanged(string p)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(p));
+        }
+
+        public string this[string columnName] => throw new NotImplementedException();
+
+        public string Error { get 
+            {
+                ValidationContext ValCon = new ValidationContext(this) { MemberName = columnName };
+                List<ValidationResult> ListaErrori = new List<ValidationResult>();
+
+                if (Validator.TryValidateProperty(
+                    GetType().GetProperty(columnName).GetValue(this),
+                    ValCon,
+                    ListaErrori
+                    ))
+                    return "";
+                return ListaErrori.First().ErrorMessage;
+                
+            } 
         }
     }
 }
